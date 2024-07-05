6eb631ceb4cbfcbaad6b3ce926cb880378c1beeb
skanderkaroui <skander.karoui@gmail.com>
2024-07-05 10:45:44 +0100

    reformating the file

--- source/services/capture_voice.py
+++ source/services/capture_voice.py
@@ -5,8 +5,14 @@ from queue import Queue
 from faster_whisper import WhisperModel
 import torch
 
+
+# Function to transcribe a chunk of audio
+def transcribe_chunk(model, file_path):
+    segments, info = model.transcribe(file_path)
+    return ' '.join(segment.text for segment in segments)
+
 # Function to record a chunk of audio
-def record_chunk(p, stream, file_path, chunk_length):
+def record_chunk(p, stream, file_path, chunk_length=1):
     frames = []
     for _ in range(0, int(16000 / 1024 * chunk_length)):
         data = stream.read(1024)
@@ -19,11 +25,6 @@ def record_chunk(p, stream, file_path, chunk_length):
     wf.writeframes(b''.join(frames))
     wf.close()
 
-# Function to transcribe a chunk of audio
-def transcribe_chunk(model, file_path):
-    segments, info = model.transcribe(file_path)
-    return ' '.join(segment.text for segment in segments)
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f'Using device: {device}')
 
@@ -33,20 +34,24 @@ def capture_voice(queue: Queue):
     model = WhisperModel(model_size, device=device, compute_type="float16")
 
     p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1, rate=16000, input=True, frames_per_buffer=1024)
+    stream = p.open(formatranscribe_chunkt=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+
+    accumulated_transcription = ""
 
     try:
         while True:
             chunk_file = "temp_chunk.wav"
             record_chunk(p, stream, chunk_file, chunk_length=1)  # Adjust chunk length as needed
-            transcription = transcribe_chunk(model, chunk_file)
+            transcription = (model, chunk_file)
             print(transcription)
             os.remove(chunk_file)
             queue.put(transcription)
     except KeyboardInterrupt:
         print("Stopping...")
+        with open("log.txt", "w") as log_file:
+            log_file.write(accumulated_transcription)
     finally:
+        print("LOG:" + accumulated_transcription)
         stream.stop_stream()
         stream.close()
         p.terminate()
