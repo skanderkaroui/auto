d751cc44dfc74ff8267616b3da7b4e7f810f1b17
skanderkaroui <skander.karoui@gmail.com>
2024-07-08 15:25:00 +0100

    commented the audio

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ print(f"Using device: {device}")
 print(torch.cuda.is_available())
 print(torch.cuda.device_count())
 print(torch.cuda.current_device())
+print(torch.cuda.list_gpu_processes())
 
-import pyaudio
-
-p = pyaudio.PyAudio()
-
-for i in range(p.get_device_count()):
-    print(f"Device {i}: {p.get_device_info_by_index(i)['name']}")
-
-p.terminate()
+# import pyaudio
+#
+# p = pyaudio.PyAudio()
+#
+# for i in range(p.get_device_count()):
+#     print(f"Device {i}: {p.get_device_info_by_index(i)['name']}")
+#
+# p.terminate()
 
 if __name__ == "__main__":
     pass
