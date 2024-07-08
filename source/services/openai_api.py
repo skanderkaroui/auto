85aa621392acf7f9a92a66fd4099bfa22a03ad97
skanderkaroui <skander.karoui@gmail.com>
2024-07-08 09:44:14 +0100

    openai integration fixex

--- source/services/openai_api.py
+++ source/services/openai_api.py
@@ -1,23 +1,27 @@
-import openai
 import os
+from openai import AzureOpenAI
 
 
 class OpenAIAPI:
     def __init__(self):
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.azure_client = AzureOpenAI(
+            api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+            azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+            api_version="2024-02-01"
+        )        
         self.MODEL_NAME = "gpt-3.5-turbo"
         self.MAX_TOKENS = 2000
 
     def text_proofreading(self, text: str):
-        response = openai.ChatCompletion.create(
+        chat_client = self.azure_client.get_chat_client("my-gpt-35-turbo-deployment")
+        response = chat_client.create_completion(
             model=self.MODEL_NAME,
-            max_tokens=self.MAX_TOKENS,
             messages=[
                 {
                     "role": "system",
                     "content": "Please proofread. Please return only the proofreading results.",
                 },
                 {"role": "user", "content": text},
-            ],
+            ]
         )
         return response.choices[0]["message"]["content"].strip()
