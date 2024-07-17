f367529c8c6c93a3e7d79244552196ac7d59baa5
skanderkaroui <skander.karoui@gmail.com>
2024-07-17 11:14:34 +0100

    updated event listener

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
-chrome.tabs.onUpdated.addEventListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("meet.google.com")) {
-    const queryParameters = tab.url.split("/")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-  }
+chrome.runtime.onInstalled.addEventListener(() => {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'meet.google.com'},
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowAction()]
+        }]);
+    });
 });
